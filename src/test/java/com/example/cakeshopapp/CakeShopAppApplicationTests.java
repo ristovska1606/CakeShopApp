57e9706124f5090e4cca7623b9bb55e2d8cd122a
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-06-19 16:23:52 +0200

    Initial commit

---
+++ src/test/java/com/example/cakeshopapp/CakeShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cakeshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
