74feb02f0a7c6b8a5974994395ee57e82bd91a60
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-06-20 19:14:28 +0200

    updated dependencies

--- src/main/java/com/example/cakeshopapp/CakeShopAppApplication.java
+++ src/main/java/com/example/cakeshopapp/CakeShopAppApplication.java
@@ -2,12 +2,18 @@ package com.example.cakeshopapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @ServletComponentScan
+@EntityScan("com.example.cakeshopapp")
+@EnableJpaRepositories("com.example.cakeshopapp")
+@Configuration
 public class CakeShopAppApplication {
 
     public static void main(String[] args) {
